b0b90e11c3950c527d4b52254416bd972f35305f
Maks <pvl1720@gmail.com>
2017-03-30 19:09:02 -0700

    did the first TODO in article js file with this properties

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -6,7 +6,14 @@ function Article (opts) {
   // TODO: Use the JS object passed in to complete this constructor function:
   // Save ALL the properties of `opts` into `this`
   // lots of this.someProperty = opts.property
+  this.title = opts.title;
+  this.category = opts.category;
+  this.author = opts.author;
+  this.authorUrl = opts.authorUrl;
+  this.publishedOn = opts.publishedOn;
+  this.body = opts.body;
 }
+
 
 Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
